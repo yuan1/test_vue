416ce65dd10593b59b59d41db03b9ddd1a9e4627
limy <limingyuan1996@gmail.com>
2019-04-02 11:22:34 +0800

    first commit

---
+++ src/components/ChatList.vue
@@ -0,0 +1,13 @@
+<template>
+    $END$
+</template>
+
+<script>
+    export default {
+        name: "ChatList"
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/TestTransition.vue
@@ -0,0 +1,13 @@
+<template>
+    $END$
+</template>
+
+<script>
+    export default {
+        name: "TestTransition"
+    }
+</script>
+
+<style scoped>
+
+</style>
